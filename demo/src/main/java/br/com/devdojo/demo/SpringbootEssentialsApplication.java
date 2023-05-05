a99d318bc14af32d07edb4e45437714726bfdefb
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-05-04 21:06:40 -0300

    Inicio do projeto

---
+++ demo/src/main/java/br/com/devdojo/demo/SpringbootEssentialsApplication.java
@@ -0,0 +1,13 @@
+package br.com.devdojo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEssentialsApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/br/com/devdojo/demo/SpringbootEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devdojo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
