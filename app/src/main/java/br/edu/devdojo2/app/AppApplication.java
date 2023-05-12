71c43544cb1c1b545bcf37d530f2718a8e042194
Eduardo Watanabe Murakoshi <eduardowmu@yahoo.com.br>
2023-05-12 11:14:22 -0300

    curso finalizado, porem falta terminar de implementar

--- app/src/main/java/br/edu/devdojo2/app/AppApplication.java
+++ app/src/main/java/br/edu/devdojo2/app/AppApplication.java
@@ -1,11 +1,23 @@
 package br.edu.devdojo2.app;
 
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
+	/*
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application",applicationName);
+	}
+	*/
 }
